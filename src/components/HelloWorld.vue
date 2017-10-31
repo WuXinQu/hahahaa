0ebd7fdcf883c2b9e7a3fa6e7bfe4df754c7be99
daimengyuan <2917592984@qq.com>
2017-10-31 20:55:38 +0800

    change

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test"> {{msg}} {{age}}</div>
+ 
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      age:18
+    }
+  }
+}
+</script>
+
+<style scoped>
+	.test{
+		background:red
+	}
+</style>
